2c55ba031ceeb3d305d1f5ff061a827e6b65623f
Justin <Justin@137.155.241.45>
2012-05-03 17:37:52 -0400

    test

---
+++ CeCapstoneTest/src/com/ceCapstone/test/CeCapstoneActivityTest.java
@@ -0,0 +1,23 @@
+package com.ceCapstone.test;
+
+import com.ceCapstone.CeCapstoneActivity;
+
+import android.test.*;
+import org.junit.*;
+
+public class CeCapstoneActivityTest extends AndroidTestCase {
+
+	public CeCapstoneActivityTest() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Before 
+
+	public void setUp() throws Exception {} 
+
+	@After 
+
+	public void tearDown() throws Exception {} 
+
+
+}
