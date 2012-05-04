52af0d3f00bea0bf9cee273376c9cbeaa0d19c73
Justin <Justin@192.168.1.13>
2012-05-03 22:38:03 -0400

    Added Google Maps functionality

--- CeCapstone/src/com/ceCapstone/CeCapstoneActivity.java
+++ CeCapstone/src/com/ceCapstone/CeCapstoneActivity.java
@@ -1,15 +1,23 @@
 package com.ceCapstone;
 
-import android.app.Activity;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
 import android.os.Bundle;
 
-public class CeCapstoneActivity extends Activity {
+public class CeCapstoneActivity extends MapActivity {
     /** Called when the activity is first created. */
+	@Override
+	protected boolean isRouteDisplayed(){
+		return false;
+	}
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        MapView mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
     }
     
-    //test back to local
 }
